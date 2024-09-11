9a8638ab0721ebb92d4de3c5e21cb38dcf7d28aa
kamau <githuakamau99@gmail.com>
2024-09-11 10:38:35 +0300

    added a simple UI with JetPack Compose and Learnt about Run blocking in Coroutines

--- app/src/main/java/com/kamau/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/kamau/kotlincoroutines/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.kamau.kotlincoroutines
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.kamau.kotlincoroutines.ui.theme.KotlinCoroutinesTheme
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
@@ -20,27 +29,56 @@ import kotlinx.coroutines.launch
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            KotlinCoroutinesTheme {
+                Surface {
+                    val navHostController = rememberNavController()
+                    NavHost(
+                        navController = navHostController,
+                        startDestination = "home"
+                    ){
+                        composable("home"){
+                            HomeScreen {
+                                navHostController.navigate("second")
+                            }
+                        }
 
-        GlobalScope.launch {
-            getApiData()
-            getApiData2()
+                        composable("second") {
+                            SecondScreen {
+                                Intent(
+                                    this@MainActivity, NewActivity::class.java
+                                ).also {
+                                    startActivity(it)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
 
-    }
-
-    private suspend fun getApiData() {
-
-        Log.d("tag_coroutine", "start getting data 1")
-        delay(5000)
-        Log.d("tag_coroutine", "got data 1")
-
-    }
-    private suspend fun getApiData2() {
-
-        Log.d("tag_coroutine", "start getting data 2")
-        delay(5000)
-        Log.d("tag_coroutine", "got data 2")
 
     }
 }
 
+@Composable
+fun HomeScreen(onGoClick: () -> Unit) {
+    Box(modifier = Modifier
+        .fillMaxSize(),
+        contentAlignment = Alignment.Center){
+        Button(onClick = { onGoClick() }) {
+                Text(text = "Go")
+        }
+    }
+}
+
+@Composable
+fun SecondScreen(onGoClick: () -> Unit) {
+    Box(modifier = Modifier
+        .fillMaxSize(),
+        contentAlignment = Alignment.Center){
+        Button(onClick = { onGoClick() }) {
+            Text(text = "Go to new Activity")
+        }
+    }
+}
---
+++ app/src/main/java/com/kamau/kotlincoroutines/NewActivity.kt
@@ -0,0 +1,9 @@
+package com.kamau.kotlincoroutines
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+class NewActivity(){
+
+}
